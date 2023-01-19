5cfd54fd5721f1d9ff629d12841eef1c690139c7
ars-qb <arssai.main@gmail.com>
2023-01-19 14:19:35 +0600

    Lab1

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -336,3 +336,61 @@ if 5 > 2: print("Five is greater than two!")
 # 9
 print("Yes") if 5 > 2 else print("No")
 
+
+### Python Loops
+# 1
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+# 2
+i = 1
+while i < 6:
+  if i == 3:
+    break
+  i += 1
+
+# 3
+i = 0
+while i < 6:
+  i += 1
+  if i == 3:
+    continue
+  print(i)
+
+# 4
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+else:
+  print("i is no longer less than 6")
+
+
+### Python For Loops
+# 1
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+# 2
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    continue
+
+  print(x)
+
+# 3
+for x in range(6):
+  print(x)
+
+# 4
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    break
+
+  print(x)
+
+
